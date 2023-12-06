ed40bfc1ca1f2ebc43dc6f3db8718f44cfbfb770
Bas Dingemans <bas@dingemans.tk>
2023-12-06 22:09:16 +0100

    Made re-order call the right methods via reflection instead of doing it via calling the event handlers

--- Editor/NdmfVrcReorder.cs
+++ Editor/NdmfVrcReorder.cs
@@ -3,14 +3,15 @@ using System.Reflection;
 using VRC.SDKBase.Editor.BuildPipeline;
 using UnityEditor;
 using System.Collections.Generic;
-using System.Linq;
 using nadena.dev.ndmf;
 using tk.dingemans.bigibas123.NdmfVrcfReorder;
 using UnityEngine;
 using VRC.SDK3A.Editor;
 using tk.dingemans.bigibas123.NdmfVrcfReorder.Extensions;
 using UnityEngine.SceneManagement;
+using VF.Builder;
 using VF.Builder.Exceptions;
+using VF.Builder;
 
 [assembly: ExportsPlugin(typeof(VrcfReorderedPlugin))]
 
@@ -27,135 +28,28 @@ namespace tk.dingemans.bigibas123.NdmfVrcfReorder
                 .AfterPlugin("com.anatawa12.avatar-optimizer")
                 .Run("VRCFury but at a non-default time", ctx =>
                 {
-                    ctx.AvatarRootObject.name += "(Clone)";
-                    bool result = Fixer.Instance.callVRCF(ctx.AvatarRootObject);
-                    if (!result)
+                    if (!Application.isPlaying)
                     {
-                        throw new VRCFBuilderException(
-                            "Error building VRCF from Reordered position please check log for details");
-                    }
-                });
-        }
-    }
-
-    // public class BeforeAllHook : IVRCSDKPreprocessAvatarCallback
-    // {
-    //     //  Hopefully before everything keeping the list stable as it progresses
-    //     public int CallbackOrder = int.MinValue;
-    //     public int callbackOrder => CallbackOrder;
-    //
-    //     public bool OnPreprocessAvatar(GameObject avatarGameObject)
-    //     {
-    //         this.Print("OnPreprocessAvatar before\n" + Fixer.Instance.GetAllContentsAsString());
-    //         Fixer.Instance.fixList();
-    //         this.Print("OnPreprocessAvatar after\n" + Fixer.Instance.GetAllContentsAsString());
-    //         return true;
-    //     }
-    // }
-    //
-    // public class NormalHook : IVRCSDKPreprocessAvatarCallback
-    // {
-    //     // Before RemoveAvatarEditorOnly and other VRCSDK callbacks after most stuff has happened
-    //     // but in roughly the same time as the original ndmf hook
-    //     public int CallbackOrder = -1040;
-    //     public int callbackOrder => CallbackOrder;
-    //
-    //     public bool OnPreprocessAvatar(GameObject avatarGameObject)
-    //     {
-    //         this.Print("OnPreprocessAvatar before\n" + Fixer.Instance.GetAllContentsAsString());
-    //         Fixer.Instance.fixList();
-    //         this.Print("OnPreprocessAvatar after\n" + Fixer.Instance.GetAllContentsAsString());
-    //         //TODO return Fixer.Instance.callVRCF(avatarGameObject);
-    //         return true;
-    //     }
-    // }
-
-    public class NdmfVrcfReorderOnSdkAttempt
-    {
-        [InitializeOnLoadMethod]
-        static void go()
-        {
-            VRCSdkControlPanel.OnSdkPanelEnable += (sender, e) =>
-            {
-                Fixer.Instance.fixList();
-                if (VRCSdkControlPanel.TryGetBuilder<IVRCSdkAvatarBuilderApi>(out var builder))
-                {
-                    builder.OnSdkBuildStart += (sender2, target) => { Fixer.Instance.fixList(); };
-                }
-            };
-        }
-
-        public static void Print(string arg)
-        {
-            int lastDot = typeof(NdmfVrcfReorderOnSdkAttempt).Namespace.LastIndexOf(".", StringComparison.Ordinal);
-            string name = typeof(NdmfVrcfReorderOnSdkAttempt).Namespace.Substring(lastDot) + "." +
-                          nameof(NdmfVrcfReorderOnSdkAttempt);
-
-            Debug.Log("[" + name + "] " + arg);
-        }
-    }
-
-    public sealed class Fixer
-    {
-        private List<IVRCSDKPreprocessAvatarCallback> _vrcfHooks = new List<IVRCSDKPreprocessAvatarCallback>();
-
-        public void fixList()
-        {
-            var cbs = getAllPreprocessAvatarCallbacks();
-            cbs.RemoveAll(callback =>
-            {
-                switch (callback.GetType().FullName)
-                {
-                    case "VF.VrcHooks.PreuploadHook":
-                        _vrcfHooks.Add(callback);
-                        this.Print("Found VRCFury PreuploadHook");
-                        return true;
-                    default:
-                        return false;
-                }
-            });
-        }
-
-        public bool callVRCF(GameObject avatarGameObject)
-        {
-            if (!Application.isPlaying)
-            {
-                this.Print("Running vrcf Callbacks");
-                foreach (var cb in _vrcfHooks.OrderBy(callback => callback.callbackOrder))
-                {
-                    if (!cb.OnPreprocessAvatar(avatarGameObject))
-                    {
-                        string message =
-                            "The VRCSDK build was aborted in the NdmfVrcReorder because the VRCSDKPreprocessAvatarCallback '" +
-                            cb.GetType().Name + "' reported a failure.";
-                        Debug.LogError((object)message);
-                        EditorUtility.DisplayDialog("Preprocess Callback Failed", message, "Ok");
-                        return false;
+                        var builder = new VRCFuryBuilder();
+                        var method = getVRCFBuilderSafeRunMethod();
+                        bool vrcFurySuccess = (bool)method.Invoke(builder, new object[]
+                        {
+                            (VFGameObject)ctx.AvatarRootObject.asVf(),
+                            (VFGameObject) null
+                        });
+                        if (!vrcFurySuccess)
+                        {
+                            throw new VRCFBuilderException(
+                                "Error building VRCF from Reordered position please check log for details");
+                        }
                     }
                     else
                     {
-                        this.Print("Step: " + cb.GetType().FullName + " executed successfuly");
+                        var method = getVRCFPlaymodeRescanMethod();
+                        this.Print("Running method: " + method);
+                        method.Invoke(null, new object[] { ctx.AvatarRootObject.scene, LoadSceneMode.Single });
                     }
-                }
-            }
-            else
-            {
-                var method = getVRCFPlaymodeRescanMethod();
-                this.Print("Running method: "+method);
-                method.Invoke(null, new object[] { avatarGameObject.scene, LoadSceneMode.Single });
-            }
-
-            return true;
-        }
-
-        private List<IVRCSDKPreprocessAvatarCallback> getAllPreprocessAvatarCallbacks()
-        {
-            var callbackField = typeof(VRCBuildPipelineCallbacks).GetField("_preprocessAvatarCallbacks",
-                BindingFlags.DeclaredOnly | BindingFlags.GetField | BindingFlags.IgnoreCase | BindingFlags.NonPublic |
-                BindingFlags.Static);
-            List<IVRCSDKPreprocessAvatarCallback> list =
-                (List<IVRCSDKPreprocessAvatarCallback>)callbackField.GetValue(null);
-            return list;
+                });
         }
 
         private MethodInfo getVRCFPlaymodeRescanMethod()
@@ -166,24 +60,64 @@ namespace tk.dingemans.bigibas123.NdmfVrcfReorder
             return dynMethod;
         }
 
-        private Fixer()
+        private MethodInfo getVRCFBuilderSafeRunMethod()
         {
+            MethodInfo dynMethod = typeof(VRCFuryBuilder).GetMethod("SafeRun",
+                BindingFlags.DeclaredOnly | BindingFlags.IgnoreCase | BindingFlags.NonPublic |
+                BindingFlags.Instance);
+            return dynMethod;
         }
+    }
 
-        public static Fixer Instance
-        {
-            get { return Nested.instance; }
-        }
 
-        private class Nested
+    public class NdmfVrcfReorderOnSdkAttempt
+    {
+        [InitializeOnLoadMethod]
+        static void go()
         {
-            // Explicit static constructor to tell C# compiler
-            // not to mark type as beforefieldinit
-            static Nested()
+            VRCSdkControlPanel.OnSdkPanelEnable += (sender, e) =>
             {
-            }
+                fixList();
+                if (VRCSdkControlPanel.TryGetBuilder<IVRCSdkAvatarBuilderApi>(out var builder))
+                {
+                    builder.OnSdkBuildStart += (sender2, target) => { fixList(); };
+                }
+            };
+        }
 
-            internal static readonly Fixer instance = new Fixer();
+        private static List<IVRCSDKPreprocessAvatarCallback> getAllPreprocessAvatarCallbacks()
+        {
+            var callbackField = typeof(VRCBuildPipelineCallbacks).GetField("_preprocessAvatarCallbacks",
+                BindingFlags.DeclaredOnly | BindingFlags.GetField | BindingFlags.IgnoreCase | BindingFlags.NonPublic |
+                BindingFlags.Static);
+            List<IVRCSDKPreprocessAvatarCallback> list =
+                (List<IVRCSDKPreprocessAvatarCallback>)callbackField.GetValue(null);
+            return list;
+        }
+
+        public static void fixList()
+        {
+            var cbs = getAllPreprocessAvatarCallbacks();
+            cbs.RemoveAll(callback =>
+            {
+                switch (callback.GetType().FullName)
+                {
+                    case "VF.VrcHooks.PreuploadHook":
+                        Print("Found VRCFury PreuploadHook");
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+        }
+
+        public static void Print(string arg)
+        {
+            int lastDot = typeof(NdmfVrcfReorderOnSdkAttempt).Namespace.LastIndexOf(".") + 1;
+            string name = typeof(NdmfVrcfReorderOnSdkAttempt).Namespace.Substring(lastDot) + "." +
+                          nameof(NdmfVrcfReorderOnSdkAttempt);
+
+            Debug.Log("[" + name + "] " + arg);
         }
     }
 }
