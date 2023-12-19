2bb98811a7ef8db3e044f7169e92d94565f2957f
Bas Dingemans <bas@dingemans.tk>
2023-12-19 16:19:32 +0100

    Cleaned up project

---
+++ Editor/VrcfReorderedPlugin.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+using nadena.dev.ndmf;
+using tk.dingemans.bigibas123.NdmfVrcfReorder;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VF.Builder;
+using VF.Builder.Exceptions;
+
+[assembly: ExportsPlugin(typeof(VrcfReorderedPlugin))]
+namespace tk.dingemans.bigibas123.NdmfVrcfReorder
+{
+	public class VrcfReorderedPlugin : Plugin<VrcfReorderedPlugin>
+	{
+		public override string QualifiedName => "tk.dingemans.bigibas123.NdmfVrcfReorder.VrcfReorderedPlugin";
+		public override string DisplayName => "VRCFury Reordered";
+
+		private static readonly string TAG = "[VrcfReordered]";
+
+		protected override void Configure()
+		{
+			InPhase(BuildPhase.Optimizing)
+				.AfterPlugin("com.anatawa12.avatar-optimizer")
+				.AfterPlugin("nadena.dev.modular-avatar")
+				.Run("VRCFury but at a non-default time", ctx =>
+				{
+					if (!Application.isPlaying)
+					{
+						var builder = new VRCFuryBuilder();
+						var method = GetVrcfBuilderSafeRunMethod();
+						Debug.Log($"{TAG} Running upload method: {method}");
+						bool vrcFurySuccess = (bool)method.Invoke(builder, new object[]
+						{
+							ctx.AvatarRootObject.asVf(),
+							null
+						});
+						if (!vrcFurySuccess)
+						{
+							throw new VRCFBuilderException(
+								"Error building VRCF from Reordered position please check log for details");
+						}
+					}
+					else
+					{
+						var method = GetVrcfPlaymodeRescanMethod();
+						Debug.Log($"{TAG} Running play mode method: {method}");
+						method.Invoke(null, new object[] { ctx.AvatarRootObject.scene, LoadSceneMode.Single });
+					}
+				});
+		}
+
+		private MethodInfo GetVrcfPlaymodeRescanMethod()
+		{
+			MethodInfo dynMethod = typeof(VF.PlayModeTrigger).GetMethod("OnSceneLoaded",
+				BindingFlags.DeclaredOnly | BindingFlags.IgnoreCase | BindingFlags.NonPublic |
+				BindingFlags.Static);
+			return dynMethod;
+		}
+
+		private MethodInfo GetVrcfBuilderSafeRunMethod()
+		{
+			MethodInfo dynMethod = typeof(VRCFuryBuilder).GetMethod("SafeRun",
+				BindingFlags.DeclaredOnly | BindingFlags.IgnoreCase | BindingFlags.NonPublic |
+				BindingFlags.Instance);
+			return dynMethod;
+		}
+	}
+}
---
+++ Editor/VrcfReorderedVrcfRemover.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using VRC.SDKBase.Editor.BuildPipeline;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDKBase.Editor;
+
+namespace tk.dingemans.bigibas123.NdmfVrcfReorder
+{
+    public class VrcfReorderedVrcfRemover
+    {
+        private static readonly string TAG = "[VrcfReordered]";
+        
+        [InitializeOnLoadMethod]
+        static void go()
+        {
+            VRCSdkControlPanel.OnSdkPanelEnable += (sender, e) =>
+            {
+                FixList();
+                if (VRCSdkControlPanel.TryGetBuilder<IVRCSdkBuilderApi>(out var builder))
+                {
+                    builder.OnSdkBuildStart += (sender2, target) => { FixList(); };
+                }
+            };
+        }
+
+        private static List<IVRCSDKPreprocessAvatarCallback> GetAllPreprocessAvatarCallbacks()
+        {
+            var callbackField = typeof(VRCBuildPipelineCallbacks).GetField("_preprocessAvatarCallbacks",
+                BindingFlags.DeclaredOnly | BindingFlags.GetField | BindingFlags.IgnoreCase | BindingFlags.NonPublic |
+                BindingFlags.Static);
+            List<IVRCSDKPreprocessAvatarCallback> list =
+                (List<IVRCSDKPreprocessAvatarCallback>)callbackField?.GetValue(null);
+            return list;
+        }
+
+        private static void FixList()
+        {
+            var cbs = GetAllPreprocessAvatarCallbacks();
+            if (cbs is null)
+            {
+                Debug.LogError($"{TAG} Could not find PreprocessAvatarCallbacks list, this probably means that VRCSDK has updated but this plugin hasn't");
+                return;
+            }
+
+            cbs.RemoveAll(callback =>
+            {
+                switch (callback.GetType().FullName)
+                {
+                    case "VF.VrcHooks.PreuploadHook":
+                        Debug.Log($"{TAG} Found VRCFury PreuploadHook");
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+        }
+    }
+}
